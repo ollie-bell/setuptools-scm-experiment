3829a5aa773dccd7de31e0f54b8303b57df58955
ollie-bell <56110893+ollie-bell@users.noreply.github.com>
2023-04-05 12:56:47 +0100

    adding more functions

--- src/mypackage/mymodule.py
+++ src/mypackage/mymodule.py
@@ -5,6 +5,10 @@ def hello():
     print("Hello, world!")
 
 
+def hi(name):
+    print(f"Hi, {name}")
+
+
 if __name__ == "__main__":
     payload = {'key1': 'value1', 'key2': 'value2'}
     r = requests.get('https://httpbin.org/get', params=payload)
