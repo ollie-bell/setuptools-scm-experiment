b3804cf9b7ddb28f92306405350d64c8d82e04cd
ollie-bell <56110893+ollie-bell@users.noreply.github.com>
2023-04-05 12:31:46 +0100

    add new function

--- src/mypackage/mymodule.py
+++ src/mypackage/mymodule.py
@@ -1,6 +1,12 @@
 import requests
 
+
+def hello():
+    print("Hello, world!")
+
+
 if __name__ == "__main__":
     payload = {'key1': 'value1', 'key2': 'value2'}
     r = requests.get('https://httpbin.org/get', params=payload)
+    hello()
     print(r.url)
